a7df1c0f6142ace04317b75d1a4d6292e728a969
Min <als904204@naver.com>
2023-10-16 22:16:09 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+let count = ref(0);
+function handleClickAdd() {
+  count.value++;
+}
+</script>
+
+<template>
+  <button v-on:click="handleClickAdd">+</button>
+  <p>{{count}}</p>
+  <HelloWorld msg=""></HelloWorld>
+</template>
+
+<style scoped>
+
+</style>
